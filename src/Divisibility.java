d07154b6cde70ac391dcb72468d5f19a18a7481a
ansfridpereira <89774958+ansfridpereira@users.noreply.github.com>
2021-09-22 19:54:00 +0530

    Is number divisible by 2

---
+++ src/Divisibility.java
@@ -0,0 +1,11 @@
+public class Divisibility {
+    public static void main(String [] args)
+    {int num = 14;
+        if(num%2 == 0) {
+            System.out.println("Number is divisible by 2");
+        }
+        else {
+            System.out.println("Number is not divisible by 2");
+        }
+    }
+}
